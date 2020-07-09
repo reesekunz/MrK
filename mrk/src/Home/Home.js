a727354b174cac0d490dce4d71700bf18a0084d6
Reese Kunz <reesekunz21@gmail.com>
2020-07-08 20:16:31 -0700

    home page styling

--- mrk/src/Home/Home.js
+++ mrk/src/Home/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Home.scss";
-
+import WelcomeMat from "./WelcomeMat";
 function Home() {
   return (
     <div className="home-container">
       <div className="stage">
-        <div className="threed floor"></div>
+        <div className="threed floor">
+          <WelcomeMat />
+        </div>
         <div className="threed leftwall "></div>
         <div className="threed rightwall "></div>
-        <div className="threed center ">Mr Kunz</div>
+        <div className="threed center "></div>
         <div className="threed ceiling "></div>
       </div>
     </div>
---
+++ mrk/src/Home/WelcomeMat.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./WelcomeMat.scss";
+
+function WelcomeMat() {
+  return (
+    <div className="welcome-mat-container">
+      Welcome Students 👋
+      {/* <div id="mickey">
+        <div className="ear left"></div>
+        <div className="ear right"></div>
+        <div className="head"></div>
+        <div className="face">
+          <div className="front left"></div>
+          <div className="front right"></div>
+          <div className="cheek left"></div>
+          <div className="cheek right"></div>
+          <div className="chin"></div>
+          <div className="mouth-wrapper">
+            <div className="mouth">
+              <div className="tong1"></div>
+              <div className="tong2"></div>
+            </div>
+            <div className="lip"></div>
+            <div className="line left"></div>
+            <div className="line right"></div>
+          </div>
+          <div className="eyes">
+            <div className="eye left">
+              <div className="pupil"></div>
+              <div className="eyelid-top"></div>
+              <div className="eyelid-bottom"></div>
+            </div>
+            <div className="eye right">
+              <div className="pupil"></div>
+              <div className="eyelid-top"></div>
+              <div className="eyelid-bottom"></div>
+            </div>
+          </div>
+          <div className="topnose"></div>
+          <div className="nose"></div>
+        </div>
+      </div> */}
+    </div>
+  );
+}
+
+export default WelcomeMat;
