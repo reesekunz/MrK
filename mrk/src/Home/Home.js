d0f8c02beeafd81e59e5373a3d3e5981df2604ef
Reese Kunz <reesekunz21@gmail.com>
2020-07-08 22:55:18 -0700

    added quote and whiteboard to home page

--- mrk/src/Home/Home.js
+++ mrk/src/Home/Home.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./Home.scss";
-import WelcomeMat from "./WelcomeMat";
+import Whiteboard from "./Whiteboard";
 function Home() {
   return (
     <div className="home-container">
       <div className="stage">
-        <div className="threed floor">
-          <WelcomeMat />
+        <div className="threed floor"> </div>
+        <div className="threed leftwall ">
+          <div className="quote">
+            “Success is peace of mind, which is a direct result of
+            self-satisfaction in knowing you did your best to become the best
+            you are capable of becoming.”
+            <div className="quote-by">- John Wooden</div>
+          </div>
         </div>
-        <div className="threed leftwall "></div>
         <div className="threed rightwall "></div>
-        <div className="threed center "></div>
+        <div className="threed center ">
+          <Whiteboard />
+        </div>
         <div className="threed ceiling "></div>
       </div>
     </div>
---
+++ mrk/src/Home/Whiteboard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Whiteboard.scss";
+
+function Whiteboard() {
+  return (
+    <div className="whiteboard-container">
+      <div id="content">
+        <div id="white-board">
+          <div id="text">
+            <span>Welcome to Mr. Kunz's Classroom</span>
+          </div>
+
+          <div className="whiteboard-content">
+            <div className="tabs">Social Studies | Math | School Events</div>
+          </div>
+
+          <div id="eraser"></div>
+          <div id="red-pen"></div>
+          <div id="blue-pen"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Whiteboard;
