6a4337d32a5968b7690209631835481beddb714b
Reese Kunz <reesekunz21@gmail.com>
2020-07-23 12:47:05 -0700

    set up emailjs with contact form

--- mrk/src/Contact/Contact.js
+++ mrk/src/Contact/Contact.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Contact.scss";
 import Hamburger from "../util/Hamburger";
+import EmailForm from "./EmailForm";
 
 function Contact() {
   return (
     <div className="contact-container">
       <Hamburger />
-      <h3 className="subject-header">Contact Mr. Kunz</h3>
-      Contact Page
+      <h3 className="subject-header">Contact Me</h3>
+      <EmailForm />
+
+      <div className="thank-you-message">THXXXXXXX FAM</div>
     </div>
   );
 }
