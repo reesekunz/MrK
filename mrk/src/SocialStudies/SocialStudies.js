931e44f62f52df1d96ab64a58ebe82958f5b4a96
Reese Kunz <reesekunz21@gmail.com>
2020-07-29 12:46:43 -0700

    finished social studies page desktop layout

--- mrk/src/SocialStudies/SocialStudies.js
+++ mrk/src/SocialStudies/SocialStudies.js
@@ -13,6 +13,80 @@ function SocialStudies() {
           "Veni, vidi, vici." - Julius Caesar{" "}
         </p>
       </div>
+      <div className="content-wrapper">
+        <div className="row">
+          {/* Current Topic */}
+          <div className="box">
+            <h2 className="box-header">Current Topic</h2>
+            <img
+              className="box-img"
+              src="https://img.icons8.com/dusk/64/000000/classroom.png"
+            />
+          </div>
+          {/* Projects */}
+          <div className="box">
+            {" "}
+            <h2 className="box-header">Projects</h2>
+            <img
+              className="box-img"
+              // src="https://img.icons8.com/dusk/64/000000/group-of-projects.png"
+              src="https://img.icons8.com/dusk/64/000000/paper-plane.png"
+            />
+            {/* <img src="https://img.icons8.com/color/48/000000/project-management.png" />{" "} */}
+          </div>
+          {/* Key Vocab Terms */}
+          <div className="box">
+            {" "}
+            <h2 className="box-header">Vocabulary Terms</h2>
+            <img
+              className="box-img"
+              src="https://img.icons8.com/cotton/64/000000/marker-pen.png"
+            />{" "}
+          </div>
+        </div>
+        <div className="row">
+          {/* Assignments */}
+          <div className="box">
+            {" "}
+            <h2 className="box-header">Assignments</h2>
+            <img
+              className="box-img"
+              // src="https://img.icons8.com/clouds/100/000000/school.png"
+              src="https://img.icons8.com/bubbles/100/000000/school.png"
+              src="https://img.icons8.com/plasticine/100/000000/school.png"
+              src="https://img.icons8.com/doodle/48/000000/books.png"
+            />
+          </div>
+          {/* Upcoming Assessments */}
+          <div className="box">
+            <h2 className="box-header">Assessments</h2>
+            <img
+              className="box-img"
+              src="https://img.icons8.com/dusk/64/000000/exam.png"
+            />{" "}
+          </div>
+          {/* I.X.L. */}
+          <div className="box">
+            {" "}
+            <h2 className="box-header">I.X.L.</h2>
+            <img
+              className="box-img"
+              src="https://img.icons8.com/dusk/64/000000/e-learning.png"
+            />{" "}
+          </div>
+        </div>
+      </div>
+      <div className="icon-credit">
+        Icons by: &nbsp;
+        <a
+          href="https://icons8.com"
+          target="_blank"
+          className="icon-credt-link"
+        >
+          {" "}
+          Icons8
+        </a>
+      </div>
     </div>
   );
 }
