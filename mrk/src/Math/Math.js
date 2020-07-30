71574778aee86cbf9289da6ad8cf25a90cd9e89b
Reese Kunz <reesekunz21@gmail.com>
2020-07-29 18:30:25 -0700

    math page layout work

--- mrk/src/Math/Math.js
+++ mrk/src/Math/Math.js
@@ -21,10 +21,44 @@ function MathPage() {
       <div className="content-wrapper">
         <div className="half" id="ixl">
           <h2 className="half-header">I.X.L</h2>
+          <img
+            className="math-img"
+            src="https://img.icons8.com/dusk/64/000000/e-learning.png"
+          />{" "}
+          <div className="row" id="ixl-row">
+            <div className="ixl-link">General</div>
+            <div className="ixl-link">Individualized</div>
+          </div>
         </div>
+
         <div className="half" id="how-to">
           <h2 className="half-header">How-To Videos</h2>
+          <img
+            className="math-img"
+            src="https://img.icons8.com/dusk/64/000000/video.png"
+          />
+          <div className="link-wrapper">
+            <div className="row">
+              <div className="video-link">Fractions</div>
+              <div className="video-link">Decimals</div>
+            </div>
+            <div className="row">
+              <div className="video-link">Multiplication</div>
+              <div className="video-link">Divison</div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="icon-credit">
+        Icons by: &nbsp;
+        <a
+          href="https://icons8.com"
+          target="_blank"
+          className="icon-credt-link"
+        >
+          {" "}
+          Icons8
+        </a>
       </div>
     </div>
   );
