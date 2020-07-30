ceb6dac5f150dc89aba56289646f25ecc0b193f0
Reese Kunz <reesekunz21@gmail.com>
2020-07-30 13:15:47 -0700

    updated contact me message

--- mrk/src/Contact/Contact.js
+++ mrk/src/Contact/Contact.js
@@ -10,7 +10,15 @@ function Contact() {
       <h3 className="subject-header">Contact Me</h3>
       <EmailForm />
 
-      <div className="thank-you-message">THXXXXXXX FAM</div>
+      <div className="thank-you-message">
+        <p>
+          Thank you very much for contacting me. I will respond via e-mail
+          within 24 hours during a regular school day.
+        </p>
+        <p>Keep working hard. I'm proud of you!</p>
+        <p>- Mr. Kunz</p>
+        <p>San Pasqual Valley Middle School</p>
+      </div>
     </div>
   );
 }
