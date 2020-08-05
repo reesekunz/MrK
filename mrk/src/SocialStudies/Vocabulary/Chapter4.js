cfcf44e447ba13b53d71f241a81848df11c181b9
Reese Kunz <reesekunz21@gmail.com>
2020-08-04 17:16:29 -0700

    chapters 4 and 6 vocab added

--- mrk/src/SocialStudies/Vocabulary/Chapter4.js
+++ mrk/src/SocialStudies/Vocabulary/Chapter4.js
@@ -2,7 +2,183 @@ import React from "react";
 import "./Unit.scss";
 
 function Chapter4() {
-  return <div className="chapter-container">Chapter 4</div>;
+  return (
+    <div className="chapter-container">
+      <h1 className="chapter-header">Chapter 4: The Islamic World</h1>
+      {/* Abu al-Abbas */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          Abu al-Abbas:
+        </h3>
+        <p className="definition">
+          {" "}
+          First Abbasid ruler. Ruthless. Known as “Blood Spiller”. Built capital
+          in Bagdad
+        </p>
+      </div>
+      {/* arabesque  */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          arabesque:
+        </h3>
+        <p className="definition">
+          Pattern of curved shapes and lines resembling flowers or vines
+        </p>
+      </div>
+      {/* astrolabe */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          astrolabe:
+        </h3>
+        <p className="definition">
+          Instrument created to measure and plot the position of stars
+        </p>
+      </div>
+      {/* calligraphy */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          calligraphy:
+        </h3>
+        <p className="definition">Decorative writing</p>
+      </div>
+      {/* calipharte */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          calipharte:
+        </h3>
+        <p className="definition">
+          Government ruled by a caliph. (Caliph is title given to Muslim leader)
+        </p>
+      </div>
+      {/* dynasty */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          dynasty:
+        </h3>
+        <p className="definition">
+          Family that holds political power through many generations
+        </p>
+      </div>
+      {/* Ibn Khaldun */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          Ibn Khaldun:
+        </h3>
+        <p className="definition">
+          Author of “History of the World” book, explaining dynasties
+        </p>
+      </div>
+      {/* Ibn Rushd */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          Ibn Rushd:
+        </h3>
+        <p className="definition">
+          Physician and writer that studied Greek ideas (Arisotle
+        </p>
+      </div>
+      {/* Ibn Sina */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          Ibn Sina:
+        </h3>
+        <p className="definition">Author of books on medicine and philosophy</p>
+      </div>
+      {/* imam*/}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          imam:
+        </h3>
+        <p className="definition">Religious leader</p>
+      </div>
+      {/* letter of credit*/}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          letter of credit:
+        </h3>
+        <p className="definition">
+          Written record from one bank, allows person to take money from another
+          bank
+        </p>
+      </div>
+      {/* madrasa */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          madrasa:
+        </h3>
+        <p className="definition">Religious school</p>
+      </div>
+      {/* minaret */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          minaret:
+        </h3>
+        <p className="definition">
+          Tower where people were are called for prayer
+        </p>
+      </div>
+      {/* polygamy */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          polygamy:
+        </h3>
+        <p className="definition">A marriage to more than one spouse</p>
+      </div>
+      {/* Sharia */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          Sharia:
+        </h3>
+        <p className="definition">
+          Islamic law code. “The way that leads to God”
+        </p>
+      </div>
+      {/* social mobility */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          social mobility:
+        </h3>
+        <p className="definition">Ability to rise in society</p>
+      </div>
+      {/* souk */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          souk:
+        </h3>
+        <p className="definition">marketplace</p>
+      </div>
+      {/* sultan */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          sultan:
+        </h3>
+        <p className="definition">Muslim ruler</p>
+      </div>
+      {/* textile*/}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          textile:
+        </h3>
+        <p className="definition">Woven goods</p>
+      </div>
+      {/* treaty */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          treaty:
+        </h3>
+        <p className="definition">
+          Written agreement between countries or peoples
+        </p>
+      </div>
+      {/* urban */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter4">
+          urban:
+        </h3>
+        <p className="definition">Dealing with a city or cities </p>
+      </div>
+    </div>
+  );
 }
 
 export default Chapter4;
--- mrk/src/SocialStudies/Vocabulary/Chapter6.js
+++ mrk/src/SocialStudies/Vocabulary/Chapter6.js
@@ -2,7 +2,148 @@ import React from "react";
 import "./Unit.scss";
 
 function Chapter6() {
-  return <div className="chapter-container">Chapter 6</div>;
+  return (
+    <div className="chapter-container">
+      <h1 className="chapter-header">Chapter 6: West African Civilization</h1>
+      {/* Askia Muhammad */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          Askia Muhammad:
+        </h3>
+        <p className="definition">
+          {" "}
+          Devoted Muslim that took over control of Songhai. Goal was to take
+          over more lands and spread Islam.
+        </p>
+      </div>
+      {/* caste */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          caste:
+        </h3>
+        <p className="definition">Social class</p>
+      </div>
+      {/* cavalry */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          cavalry:
+        </h3>
+        <p className="definition">Mounted troops</p>
+      </div>
+      {/* clan */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          clan:
+        </h3>
+        <p className="definition">Large group of related families </p>
+      </div>
+      {/* ethnic group */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          ethnic group:
+        </h3>
+        <p className="definition">
+          People sharing a distinct culture, language, and identity
+        </p>
+      </div>
+      {/* Gao */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          Gao:
+        </h3>
+        <p className="definition">
+          Capital city of Songhai. Popular city along trading route
+        </p>
+      </div>
+      {/* griot */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          griot:
+        </h3>
+        <p className="definition">
+          Professional storytellers and oral historians
+        </p>
+      </div>
+      {/* infantry */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          infantry:
+        </h3>
+        <p className="definition">Foot soldiers</p>
+      </div>
+      {/* kinship */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          kinship:
+        </h3>
+        <p className="definition">
+          Connections based upon family relationships
+        </p>
+      </div>
+      {/* lineage */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          lineage:
+        </h3>
+        <p className="definition">
+          Group of people related to a common ancestor
+        </p>
+      </div>
+      {/* Songhai*/}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          Songhai:
+        </h3>
+        <p className="definition">Muslim kingdom in the 1300’s</p>
+      </div>
+      {/* polyrhythmic*/}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          polyrhythmic:
+        </h3>
+        <p className="definition">
+          Playing two or more different rhythms at the same time
+        </p>
+      </div>
+      {/* proverb */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          proverb:
+        </h3>
+        <p className="definition">
+          Wise sayings. Gave a way to pass along wisdom through story telling
+        </p>
+      </div>
+      {/* siege */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          siege:
+        </h3>
+        <p className="definition">
+          Military attack that uses strategy of cutting off supplies
+        </p>
+      </div>
+      {/* Sunni Ali Ber */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          Sunni Ali Ber:
+        </h3>
+        <p className="definition">
+          “Ali the Great” Muslim/great military leader. Extended Songhai’s
+          empire along the Niger River’s Great Bend. Became King of Songhai.
+        </p>
+      </div>
+      {/* tyrant */}
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter6">
+          tyrant:
+        </h3>
+        <p className="definition">
+          Cruel ruler that uses their power without limits
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Chapter6;
