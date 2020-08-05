0405561a4b7e443aff31bd86cdf11a5ed0ac115d
Reese Kunz <reesekunz21@gmail.com>
2020-08-05 16:25:12 -0700

    chapters 7 and 8 vocab added

--- mrk/src/SocialStudies/Vocabulary/Chapter7.js
+++ mrk/src/SocialStudies/Vocabulary/Chapter7.js
@@ -2,7 +2,129 @@ import React from "react";
 import "./Unit.scss";
 
 function Chapter7() {
-  return <div className="chapter-container">Chapter 7</div>;
+  return (
+    <div className="chapter-container">
+      <h1 className="chapter-header">Chapter 7: The Mayas</h1>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          alliance:
+        </h3>
+        <p className="definition">
+          {" "}
+          Agreement between people, states or countries to cooperate to achieve
+          a common goal .
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          codex:
+        </h3>
+        <p className="definition">Ancient book that was written by hand</p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          drought:
+        </h3>
+        <p className="definition">Prolonged period of little or no rainfall </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          18 Rabbit:
+        </h3>
+        <p className="definition">
+          King of Copa’n at its greatest. Brought together artists and
+          architects to create amazing monuments for his city.{" "}
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          elite:
+        </h3>
+        <p className="definition">
+          Small group of highly talented or skilled people
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          extended family:
+        </h3>
+        <p className="definition">
+          Basic social unit of Maya society. Grouping of several related
+          families that lived together
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          Frederick Catherwood:
+        </h3>
+        <p className="definition">
+          British artist that drew Maya ruins and helped teach the world
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          hieroglyph:
+        </h3>
+        <p className="definition">
+          System of language in which a symbol stands for a word, idea, or sound
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          John L. Stephens:
+        </h3>
+        <p className="definition">
+          American explorer, worked with Catherwood. Discovered Maya ruins and
+          wrote about them
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          Mesoamerica:
+        </h3>
+        <p className="definition">
+          Region extending from central Mexico into northern Central America.
+          Home of the Mayas
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          Pacal:
+        </h3>
+        <p className="definition">
+          Great Maya king. Ruled for almost 70 years. Expanded city’s size and
+          power{" "}
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          slash-and-burn agriculture:
+        </h3>
+        <p className="definition">
+          Clearing large plots of land by cutting down the forest and burning
+          the trees and plants to use for fertilizer
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          sphere of influence:
+        </h3>
+        <p className="definition">
+          Area that is strongly influenced or dominated by a particular ruler or
+          government
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter7">
+          stela:
+        </h3>
+        <p className="definition">
+          Carved stone slab or pillar usually created to celebrate an event or a
+          person
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Chapter7;
--- mrk/src/SocialStudies/Vocabulary/Chapter8.js
+++ mrk/src/SocialStudies/Vocabulary/Chapter8.js
@@ -2,7 +2,122 @@ import React from "react";
 import "./Unit.scss";
 
 function Chapter8() {
-  return <div className="chapter-container">Chapter 8</div>;
+  return (
+    <div className="chapter-container">
+      <h1 className="chapter-header">Chapter 8: The Incas</h1>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          administration:
+        </h3>
+        <p className="definition"> Management of a government or business</p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          Altiplano:
+        </h3>
+        <p className="definition">
+          High plain centered in Bolivia. Midpoint of Andes
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          Andes:
+        </h3>
+        <p className="definition">
+          Mountain range along western side of South America
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          ayllu:
+        </h3>
+        <p className="definition">
+          Group of related families living in the same location
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          census:
+        </h3>
+        <p className="definition">Population survey of an area</p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          communal property:
+        </h3>
+        <p className="definition">
+          Property that is owned and operated by the government for public use
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          diplomacy:
+        </h3>
+        <p className="definition">
+          Management of communication and relationships between people or
+          nations
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          hierarchy:
+        </h3>
+        <p className="definition">
+          System for ranking members of a group by their importance
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          Huayna Capac:
+        </h3>
+        <p className="definition">
+          Last great ruler of Incas. Empire covered five different countries.
+          Peaceful
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          mita:
+        </h3>
+        <p className="definition">Labor tax</p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          Pachacuti:
+        </h3>
+        <p className="definition">
+          Greatest of Inca rulers. Used military force and diplomacy.
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          Pampas:
+        </h3>
+        <p className="definition">Grassy plains of Argentina and Uruguay</p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          quipu:
+        </h3>
+        <p className="definition">
+          A record-keeping device made of knotted strings. Different colors for
+          different items being counted
+        </p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          terrace:
+        </h3>
+        <p className="definition">Flat strips of level land</p>
+      </div>
+      <div className="vocab-term-wrapper">
+        <h3 className="vocab-term-header" id="chapter8">
+          theology:
+        </h3>
+        <p className="definition">Religious teachings or practices</p>
+      </div>
+    </div>
+  );
 }
 
 export default Chapter8;
